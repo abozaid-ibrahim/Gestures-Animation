c0c90aba91d0022210385ed3e18fd6da564c157b
abuzeid-timore <abuzeidtimore@gmail.com>
2017-09-29 06:54:26 +0200

    Initial Commit

---
+++ iOS Gestures/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS Gestures
+//
+//  Created by abuzeid on 9/29/17.
+//  Copyright © 2017 aone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iOS GesturesTests/iOS_GesturesTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_GesturesTests.swift
+//  iOS GesturesTests
+//
+//  Created by abuzeid on 9/29/17.
+//  Copyright © 2017 aone. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Gestures
+
+class iOS_GesturesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
